f7e6e07ccbf71255d6dc95bdb60c9c0d9fc56268
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-10 15:33:48 -0300

    feat: analise da tela lista de animais

---
+++ gatitobook/src/app/animais/animal/animal.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+const API: string = 'http://localhost:3000';
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css']
+})
+
+export class AnimalComponent implements OnInit {
+
+  private urlOriginal: string = '';
+
+  //Input indica que sera recebida pelo valor digitado no HTML
+  @Input() descricao: string = '';
+
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.urlOriginal = url;
+    } else {
+      this.urlOriginal = `${API}/imgs/${url}`;
+    }
+  }
+
+  get url(): string {
+    return this.urlOriginal;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
