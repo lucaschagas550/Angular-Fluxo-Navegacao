7ecc6f5c7d7f09a07af3ee529c7d077723f34af8
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2022-12-11 11:23:20 -0300

    feat: associando a rota

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LoginGuard } from './autenticacao/login.guard';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -10,11 +12,13 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard], // Guarda de rota quem ja estiver logado ir direto para tela de animais e nao precisar realizar login novamente
   },
   {
     path: 'animais',
     loadChildren: () =>
       import('./animais/animais.module').then((m) => m.AnimaisModule),
+    canLoad: [AutenticacaoGuard], // Guarda de rota para quem estiver logado no sistemas se nao eh redirecionado
   },
 ];
 
@@ -22,4 +26,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
